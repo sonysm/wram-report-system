e55f2068e0feef67c898639ba8e6e631920a0337
sonysm <sonysum7@gmail.com>
2026-08-06 11:32:28 +0700

    add font kh moul

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
 import type { AppProps } from "next/app";
+import { Moul } from "next/font/google";
 import "../styles/globals.css";
 
+const moul = Moul({
+    weight: "400",
+    subsets: ["khmer"],
+    variable: "--font-kh-moul",
+    display: "swap",
+});
+
 export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    return (
+        <div className={moul.variable}>
+            <Component {...pageProps} />
+        </div>
+    );
 }
